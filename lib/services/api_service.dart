14ce3d09882f9b9c95947d1bfd2452a0dbdc356c
Dawid Zyluk <dawid@typoedition.com>
2023-06-08 09:39:41 +0200

    Get values in descending order of timestamps

--- lib/services/api_service.dart
+++ lib/services/api_service.dart
@@ -13,19 +13,19 @@ class APIService {
   // }
 
   static Future<DataResponse> fetchPhase(String phase) async {
-    final response = await http.get(Uri.parse("${dotenv.env['DATABASE_URL2']!}/exec?query=SELECT%20*%20FROM%20phase%20WHERE%20phase%3D'$phase'&nm=true&limit=10"));
+    final response = await http.get(Uri.parse("${dotenv.env['DATABASE_URL2']!}/exec?query=SELECT%20*%20FROM%20phase%20LATEST%20BY%20ts%20WHERE%20%20phase%3D'$phase'%20ORDER%20BY%20ts%20DESC%3B&nm=true&limit=10"));
 
     return DataResponseFromJson(response.body);
   }
 
   static Future<DataResponse> fetchElectricAvg() async {
-    final response = await http.get(Uri.parse('${dotenv.env['DATABASE_URL2']!}/exec?query=SELECT * FROM electric_avg&nm=true&limit=10'));
+    final response = await http.get(Uri.parse('${dotenv.env['DATABASE_URL2']!}/exec?query=SELECT%20*%20FROM%20electric_avg%20LATEST%20BY%20ts%20ORDER%20BY%20ts%20DESC%3B&nm=true&limit=10'));
 
     return DataResponseFromJson(response.body);
   }
 
   static Future<DataResponse> fetchWaterReadings() async {
-    final response = await http.get(Uri.parse('${dotenv.env['DATABASE_URL2']!}/exec?query=SELECT * FROM panel&nm=true&limit=10'));
+    final response = await http.get(Uri.parse('${dotenv.env['DATABASE_URL2']!}/exec?query=SELECT%20*%20FROM%20panel%20LATEST%20BY%20ts%20ORDER%20BY%20ts%20DESC%3B&nm=true&limit=10'));
 
     return DataResponseFromJson(response.body);
   }
